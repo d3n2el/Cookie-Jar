16b1c371d9b089b8165daac48aebe3c32f03c5bb
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-26 14:00:59 +0200

    Create jar.py

---
+++ jar.py
@@ -0,0 +1,28 @@
+class Jar:
+    def __init__(self, capacity=12):
+        self._size= 0
+        if capacity < 0:
+            raise ValueError("Not enough cookies")
+        self.capacity = capacity
+
+    def __str__(self):
+        for len(self._size):
+            return "🍪"
+
+    def deposit(self, n):
+        if self._size+n > 12:
+            raise ValueError("Too many cookies")
+        self._size =  self._size+ n
+
+
+    def withdraw(self, n):
+        if self._size-n < 0:
+            raise ValueError("Cookies cant be negative")
+        self._size=  self._size - n
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @property
+    def size(self):
+        return self._size
